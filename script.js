f48e47be9db7fde8b547d1737bae7827bb51fdac
seku01 <rabbel.soul@gmail.com>
2024-08-28 16:15:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function inflationCalculator() {
+    let inflationRate = document.querySelector('#inflationRate');
+    let money = document.querySelector('#money');
+
+   inflationRate = parseFloat(inflationRate.value);
+   money = parseFloat(money.value);
+
+
+   let years = document.querySelector('#years').value;  
+   years = parseFloat(years);   
+   
+   let worth = money + (money * (inflationRate / 100)); 
+
+    for(let i = 1; i < years; i++) {
+        worth += worth * (inflationRate / 100);
+    }
+
+    worth = worth.toFixed(2); 
+
+    let newElement = document.createElement('div'); 
+    newElement.className = 'new-value';
+    newElement.innerText = `Todays ${money} euro will be worth ${worth} euro in ${years} years.`;
+
+    document.querySelector('.container').appendChild(newElement);
+
